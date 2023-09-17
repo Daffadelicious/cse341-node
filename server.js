a8ad2cb88cb18fc07a61ff97a3581cdcaa1f608c
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2023-09-16 23:29:32 -0700

    added render to rest woo hoo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-// Use either render from env or locally 3000
+// Use either render's port from env or locally 3000
 const port = process.env.PORT || 3000
 
 app.use('/', require("./routes"))
